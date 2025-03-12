fc4d5aa81b347f9bafe34539097575b55c27fe47
Zulridzwan Basiran <zulridzwan@gmail.com>
2025-03-12 13:31:23 +0800

    feat: Initial commit
    Testing route and static route

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	// Fiber instance
+	app := fiber.New()
+
+	// Routes
+	app.Get("/hello", hello)
+
+	app.Static("/", "./public") 
+
+	// Start server
+	log.Fatal(app.Listen(":3000"))
+}
+
+// Handler
+func hello(c *fiber.Ctx) error {
+	return c.SendString("Hello, World 👋!")
+}
